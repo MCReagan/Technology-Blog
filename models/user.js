84d0c0b724952dccffa5fb18f1fecb0015f0211b
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-14 12:47:07 -0500

    minor filechange

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    async checkPassword(loginPw) {
+        return await bcrypt.compare(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            set(value) {
+                const hashedPassword = bcrypt.hashSync(value, 10);
+                this.setDataValue('password', hashedPassword);
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
