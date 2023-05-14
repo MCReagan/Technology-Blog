0eacc36edf58ac3eda64f35066286691444d60b9
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-14 14:33:01 -0500

    blog posts display correctly on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
 const isLoggedIn = require('../utils/auth');
+const { User, Blog } = require('../models')
 
 router.get('/', async (req, res) => {
-    res.render('all');
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+            ]
+        });
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+        res.render('all', { blogs });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
