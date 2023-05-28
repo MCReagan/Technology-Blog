89afc496f66d87ee8e03f7ae121396157637bf72
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-28 15:32:30 -0500

    added procfile

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const sequelize = require('../config/config');
 const { Blog, User, Comment } = require('../models');
 const withAuth = require('../utils/auth')
 
