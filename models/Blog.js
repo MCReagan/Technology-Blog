615f05246375c5c9481a87f23c2e8b5c63d39b32
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-28 16:47:41 -0500

    changed seed files

--- models/Blog.js
+++ models/Blog.js
@@ -16,7 +16,7 @@ Blog.init(
             allowNull: false,
         },
         text: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
         user_id: {
