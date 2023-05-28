2e6e01e6766e4bf6b4b11d81ac041af759c6f1e6
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-28 15:42:28 -0500

    who knows

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: process.env.SECRET,
+    secret: 'super secret secret',
     cookie: {
         maxAge: 30000,
         httpOnly: true,
